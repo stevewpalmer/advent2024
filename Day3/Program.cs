18cb2e17e582a18f87dd1ea1697337eedf14cd81
steve <steve@stevewpalmer.uk>
2024-12-03 09:07:53 +0000

    Solution for Day 3.

---
+++ Day3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Text.RegularExpressions;
+
+string input = File.ReadAllText("day3.txt");
+MatchCollection matches = Regex.Matches(input, @"mul\((\d+),(\d+)\)|do\(\)|don't\(\)");
+
+int answer1Total = 0;
+int answer2Total = 0;
+bool enabled = true;
+foreach (Match match in matches) {
+    GroupCollection groups = match.Groups;
+    if (groups[0].Value == "do()") {
+        enabled = true;
+        continue;
+    }
+    if (groups[0].Value == "don't()") {
+        enabled = false;
+        continue;
+    }
+    int result = int.Parse(groups[1].Value) * int.Parse(groups[2].Value);
+    if (enabled) {
+        answer2Total += result;
+    }
+    answer1Total += result;
+}
+
+Console.WriteLine($"Puzzle 1 answer : {answer1Total}");
+Console.WriteLine($"Puzzle 1 answer : {answer2Total}");
