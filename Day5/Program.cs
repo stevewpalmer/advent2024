7b260e6dd012f36973fcccea7ec84df1b3eec3c5
Steve Palmer <steve@stevewpalmer.uk>
2024-12-05 09:38:19 +0000

    Add Day 5.

---
+++ Day5/Program.cs
@@ -0,0 +1,23 @@
+﻿string [] data = File.ReadAllLines("puzzle.txt");
+int line = 0;
+
+List<(int, int)> rules = [];
+while (line < data.Length && !string.IsNullOrEmpty(data[line])) {
+    int[] parts = data[line++].Split("|").Select(int.Parse).ToArray();
+    rules.Add((parts[0], parts[1]));
+}
+int totalAnswer1 = 0;
+int totalAnswer2 = 0;
+while (++line < data.Length) {
+    int[] pages = data[line].Split(",").Select(int.Parse).ToArray();
+    int[] sortedPages = (int[])pages.Clone();
+    Array.Sort(sortedPages, (t1, t2) => rules.IndexOf((t2, t1)));
+    if (pages.SequenceEqual(sortedPages)) {
+        totalAnswer1 += pages[Math.Abs(pages.Length / 2)];
+    }
+    else {
+        totalAnswer2 += sortedPages[Math.Abs(sortedPages.Length / 2)];
+    }
+}
+Console.WriteLine($"Puzzle 1 answer : {totalAnswer1}");
+Console.WriteLine($"Puzzle 1 answer : {totalAnswer2}");
