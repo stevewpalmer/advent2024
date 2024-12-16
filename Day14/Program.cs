acf8ca90701dbda532e735708c10b27bd95fb749
Steve Palmer <steve@stevewpalmer.uk>
2024-12-16 19:32:57 +0000

    Day 14 solution.

---
+++ Day14/Program.cs
@@ -0,0 +1,57 @@
+﻿using System.Drawing;
+using System.Text.RegularExpressions;
+
+List<(int seconds, int factor, List<Point> robots)> factors = [];
+
+List<(Point p, Point v)> robots = File.ReadAllLines("puzzle.txt")
+    .Select(line => Regex.Matches(line, @"p=(\d+),(\d+) v=(\-*\d+),(\-*\d+)")
+        .SelectMany(m => m.Groups.Cast<Group>()
+            .SelectMany(g => g.Captures
+                .Select(c => c.Value)))
+        .Skip(1)
+        .Select(c => int.Parse(c))
+        .ToArray())
+    .Select(values => (new Point(values[0], values[1]), new Point(values[2], values[3])))
+    .ToList();
+
+int seconds = 0;
+int width = 101;
+int height = 103;
+
+while (seconds < 10402) {
+    for (int i = 0; i < robots.Count; i++) {
+        (Point p, Point v) robot = robots[i];
+        robot.p.X = (robot.p.X + robot.v.X) % width;
+        robot.p.Y = (robot.p.Y + robot.v.Y) % height;
+        if (robot.p.Y < 0) {
+            robot.p.Y += height;
+        }
+        if (robot.p.X < 0) {
+            robot.p.X += width;
+        }
+        robots[i] = robot;
+    }
+
+    factors.Add((++seconds,
+        robots.Count(r => r.p.X < width / 2 && r.p.Y < height / 2) *
+        robots.Count(r => r.p.X > width / 2 && r.p.Y < height / 2) *
+        robots.Count(r => r.p.X < width / 2 && r.p.Y > height / 2) *
+        robots.Count(r => r.p.X > width / 2 && r.p.Y > height / 2),
+        robots.Select(r => r.p).ToList()));
+}
+
+int answer1 = factors.First(r => r.seconds == 100).factor;
+int answer2 = factors.OrderBy(f => f.factor).First().seconds;
+
+// As a bonus, draw the layout with the tree!
+List<Point> tree = factors.OrderBy(f => f.factor).First().robots;
+for (int r = 0; r < height; r++) {
+    for (int c = 0; c < width; c++) {
+        int count = tree.Count(t => t.Y == r && t.X == c);
+        Console.Write(count == 0 ? "." : count.ToString());
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine($"Part 1 answer: {answer1}");
+Console.WriteLine($"Part 2 answer: {answer2}");
