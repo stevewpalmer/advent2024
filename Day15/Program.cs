344f944fc2aaf22a66c581bc327c5fe73a9a8d3f
Steve Palmer <steve@stevewpalmer.uk>
2024-12-17 23:45:46 +0000

    Day 15 part 2 solution.

--- Day15/Program.cs
+++ Day15/Program.cs
@@ -16,7 +16,7 @@ char[][] map = input.TakeWhile(l => !string.IsNullOrEmpty(l))
     .ToArray();
 
 char[] route = string.Join("", input.SkipWhile(l => !string.IsNullOrEmpty(l))
-    .Select(line => line))
+        .Select(line => line))
     .ToCharArray();
 
 Console.WriteLine($"Part 1 answer: {Solve(map)}");
@@ -41,6 +41,80 @@ char[][] Resize(char[][] map) {
     return newMap.ToArray();
 }
 
+bool TryMove(char[][] map, Point pt, int dx, int dy) {
+    pt.Offset(dx, dy);
+    char tr = map[pt.Y][pt.X];
+    if (tr == SPACE) {
+        return true;
+    }
+    if (tr == BOX) {
+        do {
+            pt.Offset(dx, dy);
+            tr = map[pt.Y][pt.X];
+        } while (tr == BOX);
+        return tr != WALL;
+    }
+    if (tr == WALL) {
+        return false;
+    }
+    if (dx == 0) {
+        if (tr == RIGHT) {
+            return TryMove(map, new Point(pt.X, pt.Y), dx, dy) && TryMove(map, new Point(pt.X - 1, pt.Y), dx, dy);
+        }
+        if (tr == LEFT) {
+            return TryMove(map, new Point(pt.X, pt.Y), dx, dy) && TryMove(map, new Point(pt.X + 1, pt.Y), dx, dy);
+        }
+    }
+    else if (dx == -1) {
+        if (tr == RIGHT) {
+            return TryMove(map, new Point(pt.X - 1, pt.Y), dx, dy);
+        }
+    }
+    else if (dx == 1) {
+        if (tr == LEFT) {
+            return TryMove(map, new Point(pt.X + 1, pt.Y), dx, dy);
+        }
+    }
+    return false;
+}
+
+void Move(char[][] map, Point pt, int dx, int dy) {
+    Point st = pt;
+    st.Offset(dx, dy);
+    char tr = map[st.Y][st.X];
+    if (tr == SPACE) {
+        (map[st.Y][st.X], map[pt.Y][pt.X]) = (map[pt.Y][pt.X], map[st.Y][st.X]);
+    }
+    else if (tr == BOX) {
+        Move(map, st, dx, dy);
+        (map[st.Y][st.X], map[pt.Y][pt.X]) = (map[pt.Y][pt.X], map[st.Y][st.X]);
+    }
+    else if (dx == 0) {
+        if (tr == RIGHT) {
+            Move(map, new Point(st.X, st.Y), dx, dy);
+            Move(map, new Point(st.X - 1, st.Y), dx, dy);
+            (map[st.Y][st.X], map[pt.Y][pt.X]) = (map[pt.Y][pt.X], map[st.Y][st.X]);
+        }
+        if (tr == LEFT) {
+            Move(map, new Point(st.X, st.Y), dx, dy);
+            Move(map, new Point(st.X + 1, st.Y), dx, dy);
+            (map[st.Y][st.X], map[pt.Y][pt.X]) = (map[pt.Y][pt.X], map[st.Y][st.X]);
+        }
+    }
+    else if (dx == -1) {
+        if (tr == RIGHT) {
+            Move(map, new Point(st.X - 1, st.Y), dx, dy);
+            (map[st.Y][st.X - 1], map[st.Y][st.X], map[pt.Y][pt.X]) = (map[st.Y][st.X], map[pt.Y][pt.X], map[st.Y][st.X - 1]);
+        }
+    }
+    else if (dx == 1) {
+        if (tr == LEFT) {
+            Move(map, new Point(st.X + 1, st.Y), dx, dy);
+            (map[st.Y][st.X + 1], map[st.Y][st.X], map[pt.Y][pt.X]) = (map[st.Y][st.X], map[pt.Y][pt.X], map[st.Y][st.X + 1]);
+        }
+    }
+}
+
 int Solve(char[][] map) {
     robot.Y = map.Select((item, index) => (item, index)).First(l => l.item.Contains(ROBOT)).index;
     robot.X = Array.FindIndex(map[robot.Y], c => c == ROBOT);
@@ -53,38 +127,20 @@ int Solve(char[][] map) {
             case '^': dy = -1; break;
             case 'v': dy = 1; break;
         }
-        Point pt = robot;
-        char tr = map[pt.Y + dy][pt.X + dx];
-        if (tr == WALL) {
-            continue;
+        if (TryMove(map, robot, dx, dy)) {
+            Move(map, robot, dx, dy);
+            robot.Offset(dx, dy);
         }
-        if (tr == BOX) {
-            do {
-                pt.Offset(dx, dy);
-            } while (map[pt.Y][pt.X] == BOX);
-            if (map[pt.Y][pt.X] == WALL) {
-                continue;
-            }
-            while (pt != robot) {
-                map[pt.Y][pt.X] = map[pt.Y - dy][pt.X - dx];
-                pt.Offset(-dx, -dy);
-            }
-            tr = SPACE;
-        }
-        if (tr == SPACE) {
-            pt.Offset(dx, dy);
-        }
-        map[robot.Y][robot.X] = SPACE;
-        map[pt.Y][pt.X] = ROBOT;
-        robot = pt;
     }
     int sum = 0;
     for (int r = 0; r < map.Length; r++) {
         for (int c = 0; c < map[r].Length; c++) {
-            if (map[r][c] == BOX) {
+            Console.Write(map[r][c]);
+            if (map[r][c] == BOX || map[r][c] == LEFT) {
                 sum += 100 * r + c;
             }
         }
+        Console.WriteLine();
     }
     return sum;
 }
