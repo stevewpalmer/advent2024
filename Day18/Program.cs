38e79bc00613197e0b628a4dae6e54703abbbd3f
steve <steve@stevewpalmer.uk>
2024-12-19 16:40:52 +0000

    Day 18 and 19 solutions

---
+++ Day18/Program.cs
@@ -0,0 +1,57 @@
+﻿using System.Drawing;
+
+int width = 71;
+int height = 71;
+
+string[] input = File.ReadAllLines("puzzle.txt");
+char[,] maze = new char[height, width];
+Point start = new(0, 0);
+Point end = new(width - 1, height - 1);
+
+int l = 0;
+while (l < 1024) {
+    int[] coords = input[l].Split(',').Select(int.Parse).ToArray();
+    maze[coords[1], coords[0]] = '#';
+    l++;
+}
+int answer1 = Walk();
+
+while (l < input.Length) {
+    int[] coords = input[l].Split(',').Select(int.Parse).ToArray();
+    maze[coords[1], coords[0]] = '#';
+    if (Walk() == int.MaxValue) {
+        break;
+    }
+    l++;
+}
+string answer2 = input[l];
+
+Console.WriteLine($"Part 1 answer: {answer1}");
+Console.WriteLine($"Part 2 answer: {answer2}");
+return;
+
+int Walk() {
+    (int dx, int dy)[] directions = [(0, -1), (1, 0), (0, 1), (-1, 0)];
+    PriorityQueue<(Point, int), int> queue = new();
+    HashSet<Point> visited = [];
+    queue.Enqueue((start, 0), 0);
+
+    int best = int.MaxValue;
+    while (queue.TryDequeue(out var element, out int cost)) {
+        (Point pt, int _) = element;
+        if (pt == end) {
+            best = cost;
+            break;
+        }
+        if (!visited.Add(pt)) {
+            continue;
+        }
+        foreach ((int dx, int dy) in directions) {
+            Point step = new(pt.X + dx, pt.Y + dy);
+            if (step is { Y: >= 0, X: >= 0 } && step.Y < height && step.X < width && maze[step.Y, step.X] != '#') {
+                queue.Enqueue((step, cost + 1), cost + 1);
+            }
+        }
+    }
+    return best;
+}
---
+++ Day19/Program.cs
@@ -0,0 +1,23 @@
+﻿string[] lines = File.ReadAllLines("puzzle.txt");
+string[] patterns = lines[0].Split(',', StringSplitOptions.TrimEntries).OrderByDescending(l => l.Length).ToArray();
+string[] designs = lines.Skip(2).Select(l => l).ToArray();
+Dictionary<string, long> cache = [];
+
+int answer1 = designs.Count(d => TryMatch(d) > 0);
+long answer2 = designs.Sum(TryMatch);
+
+Console.WriteLine($"Part 1 answer: {answer1}");
+Console.WriteLine($"Part 2 answer: {answer2}");
+return;
+
+long TryMatch(string design) {
+    if (cache.TryGetValue(design, out long count)) {
+        return count;
+    }
+    if (design.Length == 0) {
+        return 1;
+    }
+    count = patterns.Where(design.StartsWith).Sum(s => TryMatch(design[s.Length..]));
+    cache.Add(design, count);
+    return count;
+}
