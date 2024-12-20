07ad4c055eeb9b731e4e3eb1ab7d4ed9a93e418b
Steve Palmer <steve@stevewpalmer.uk>
2024-12-20 06:07:05 +0000

    Compress loc a bit more.

--- Day16/Program.cs
+++ Day16/Program.cs
@@ -16,35 +16,30 @@ for (int r = 0; r < maze.Length; r++) {
     }
 }
 
-(int answer1, int answer2) = Walk();
-Console.WriteLine($"Part 1 answer: {answer1}");
-Console.WriteLine($"Part 2 answer: {answer2}");
-return;
+PriorityQueue<(Point, int, List<Point>), int> queue = new();
+Dictionary<(Point, int), int> visited = [];
+HashSet<Point> paths = [start];
+queue.Enqueue((start, 1, []), 0);
 
-(int, int) Walk() {
-    PriorityQueue<(Point, int, List<Point>), int> queue = new();
-    Dictionary<(Point, int), int> visited = [];
-    HashSet<Point> paths = [start];
-    queue.Enqueue((start, 1, []), 0);
-
-    int best = int.MaxValue;
-    while (queue.TryDequeue(out var element, out int cost)) {
-        (Point pt, int dt, List<Point> path) = element;
-        if (visited.TryGetValue((pt, dt), out int score) && score < cost) {
-            continue;
-        }
-        visited[(pt, dt)] = cost;
-        if (pt == end && cost <= best) {
-            best = cost;
-            paths.UnionWith(path);
-        }
-        foreach ((int d, int c) in ((int, int)[]) [(1, 1001), (-1, 1001), (0, 1)]) {
-            int nd = (dt + d % 4 + 4) % 4;
-            Point step = new(pt.X + directions[nd].dx, pt.Y + directions[nd].dy);
-            if (maze[step.Y][step.X] != '#') {
-                queue.Enqueue((step, nd, [.. path, step]), cost + c);
-            }
+int best = int.MaxValue;
+while (queue.TryDequeue(out var element, out int cost)) {
+    (Point pt, int dt, List<Point> path) = element;
+    if (visited.TryGetValue((pt, dt), out int score) && score < cost) {
+        continue;
+    }
+    visited[(pt, dt)] = cost;
+    if (pt == end && cost <= best) {
+        best = cost;
+        paths.UnionWith(path);
+    }
+    foreach ((int d, int c) in ((int, int)[]) [(1, 1001), (-1, 1001), (0, 1)]) {
+        int nd = (dt + d % 4 + 4) % 4;
+        Point step = new(pt.X + directions[nd].dx, pt.Y + directions[nd].dy);
+        if (maze[step.Y][step.X] != '#') {
+            queue.Enqueue((step, nd, [.. path, step]), cost + c);
         }
     }
-    return (best, paths.Count);
 }
+
+Console.WriteLine($"Part 1 answer: {best}");
+Console.WriteLine($"Part 2 answer: {paths.Count()}");
