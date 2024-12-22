9b0a24c71995665c86b6496353f983228ef03b9a
Steve Palmer <steve@stevewpalmer.uk>
2024-12-22 10:45:35 +0000

    Finally solved Day 17 part 2.

--- Day17/Program.cs
+++ Day17/Program.cs
@@ -1,8 +1,7 @@
-﻿int regA = 0;
-int regB = 0;
-int regC = 0;
-int[] data = [];
-List<int> outp = [];
+﻿long regA = 0;
+long regB = 0;
+long regC = 0;
+List<int> data = [];
 
 foreach (string input in File.ReadLines("puzzle.txt")) {
     string[] parts = input.Split(":");
@@ -10,20 +9,21 @@ foreach (string input in File.ReadLines("puzzle.txt")) {
         case "Register A": regA = int.Parse(parts[1]); break;
         case "Register B": regB = int.Parse(parts[1]); break;
         case "Register C": regC = int.Parse(parts[1]); break;
-        case "Program": data = parts[1].Split(",").Select(int.Parse).ToArray(); break;
+        case "Program": data = parts[1].Split(",").Select(int.Parse).ToList(); break;
     }
 }
 
-string answer1 = Run(regA);
 Disassemble(data);
 
+string answer1 = string.Join(",", Run(regA, regB, regC));
+long answer2 = Solve(0, 0);
+
 Console.WriteLine($"Part 1 answer: {answer1}");
+Console.WriteLine($"Part 2 answer: {answer2}");
 return;
 
-int combo(int operand) => ((int[]) [0, 1, 2, 3, regA, regB, regC])[operand];
-
-void Disassemble(int[] program) {
-    for (int pc = 0; pc < program.Length;) {
+void Disassemble(List<int> program) {
+    for (int pc = 0; pc < program.Count;) {
         int opcode = program[pc];
         int operand = program[pc + 1];
         string op2 = ((string[]) ["0", "1", "2", "3", "A", "B", "C"])[operand];
@@ -42,39 +42,56 @@ void Disassemble(int[] program) {
     }
 }
 
-string Run(int input) {
-    regA = input;
-    for (int pc = 0; pc < data.Length;) {
+long Solve(long a, int depth) {
+    if (depth == data.Count) {
+        return a;
+    }
+    for (int n = 0; n < 8; n++) {
+        List<int> output = Run(a * 8 + n, regB, regC);
+        if (output.Count > 0 && output[0] == data[^(depth + 1)]) {
+            long result = Solve(a * 8 + n, depth + 1);
+            if (result > 0) {
+                return result;
+            }
+        }
+    }
+    return 0;
+}
+
+List<int> Run(long a, long b, long c) {
+    List<int> output = [];
+    for (int pc = 0; pc < data.Count;) {
+        long[] combo = [0, 1, 2, 3, a, b, c, 999999];
         int opcode = data[pc++];
         int operand = data[pc++];
         switch (opcode) {
             case 0:
-                regA /= 1 << combo(operand);
+                a /= (long)Math.Pow(2, combo[operand]);
                 break;
             case 1:
-                regB ^= operand;
+                b ^= operand;
                 break;
             case 2:
-                regB = combo(operand) % 8;
+                b = combo[operand] % 8;
                 break;
             case 3:
-                if (regA > 0) {
+                if (a > 0) {
                     pc = operand;
                 }
                 break;
             case 4:
-                regB ^= regC;
+                b ^= c;
                 break;
             case 5:
-                outp.Add(combo(operand) % 8);
+                output.Add((int)(combo[operand] % 8));
                 break;
             case 6:
-                regB = regA / (1 << combo(operand));
+                b = a / (long)Math.Pow(2, combo[operand]);
                 break;
             case 7:
-                regC = regA / (1 << combo(operand));
+                c = a / (long)Math.Pow(2, combo[operand]);
                 break;
         }
     }
-    return string.Join(",", outp);
+    return output;
 }
