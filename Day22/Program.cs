2e5375e26a84b4920ffc07a9587c83094bd94ad5
Steve Palmer <steve@stevewpalmer.uk>
2024-12-22 11:05:29 +0000

    Day 22, part 1 answer.

---
+++ Day22/Program.cs
@@ -0,0 +1,18 @@
+﻿string[] input = File.ReadAllLines("puzzle.txt");
+
+long answer1 = input.Sum(l => Secret(long.Parse(l)));
+
+Console.WriteLine($"Part 1 answer: {answer1}");
+return;
+
+long Secret(long number) {
+    for (int c = 0; c < 2000; c++) {
+        number = (number * 64) ^ number;
+        number %= 16777216;
+        number = (long)Math.Floor((double)number / 32) ^ number;
+        number %= 16777216;
+        number = (number * 2048) ^ number;
+        number %= 16777216;
+    }
+    return number;
+}
