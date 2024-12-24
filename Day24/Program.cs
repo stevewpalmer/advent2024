bce97c70e3c78d7fd44f9b0192dbe4787fa18b98
Steve Palmer <steve@stevewpalmer.uk>
2024-12-24 14:43:23 +0000

    Day 24 part 1 solution.

---
+++ Day24/Program.cs
@@ -0,0 +1,34 @@
+﻿string[] input = File.ReadAllText("puzzle.txt").Split("\n\n");
+Dictionary<string, string> registers = [];
+
+foreach (string line in input[0].Split('\n')) {
+    string[] parts = line.Split(": ");
+    registers[parts[0]] = parts[1];
+}
+foreach (string line in input[1].Split('\n')) {
+    string[] parts = line.Split(" -> ");
+    registers[parts[1]] = parts[0];
+}
+
+long answer1 = Expand();
+Console.WriteLine($"Part 1 answer: {answer1}");
+return;
+
+long Expand() =>
+    registers.Keys.Where(s => s.StartsWith('z')).OrderByDescending(s => s)
+        .Aggregate<string, long>(0, (current, name) => current * 2 + Evaluate(name));
+
+int Evaluate(string name) {
+    string value = registers[name];
+    if (int.TryParse(value, out int result)) {
+        return result;
+    }
+    string[] parts = value.Split(' ');
+    int op1 = Evaluate(parts[0]);
+    int op2 = Evaluate(parts[2]);
+    return parts[1] switch {
+        "XOR" => op1 ^ op2,
+        "AND" => op1 & op2,
+        _ => op1 | op2
+    };
+}
