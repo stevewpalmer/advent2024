d2dfd6355a2deb72c5248d6edf7008fd91e7600c
Steve Palmer <steve@stevewpalmer.uk>
2024-12-25 11:17:48 +0000

    Day 25, part 1 solution.

---
+++ Day25/Program.cs
@@ -0,0 +1,25 @@
+﻿string[] sets = File.ReadAllText("puzzle.txt").Split("\n\n");
+
+List<long> locks = [];
+List<long> keys = [];
+
+foreach (string set in sets) {
+    long value = Convert.ToInt64(set.Split("\n").Aggregate("", (current, line) => current + line.Replace('.', '0').Replace('#', '1')), 2);
+    if ((value & 31) != 0) {
+        locks.Add(value);
+    } else {
+        keys.Add(value);
+    }
+}
+
+int overlaps = 0;
+int total = 0;
+foreach (long alock in locks) {
+    foreach (long akey in keys) {
+        total++;
+        if ((alock & akey) != 0) {
+            ++overlaps;
+        }
+    }
+}
+Console.WriteLine($"Part 1 answer: {total - overlaps}");
